8cd4ab22be1f73f9234fea0c0a269968d6816c85
Dawid Linek <dawid@linek.dev>
2025-01-11 00:16:24 +0100

    feat: change color palate to solvro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,19 +3,23 @@ import starlightPlugin from "@astrojs/starlight-tailwind";
 // Generated color palettes
 const accent = {
   200: "#80B3FF",
-  600: "#274276",
+  400: "#6583C8",
+  600: "#305088",
+  800: "#243D6A",
   900: "#203560",
-  950: "#203560",
+  950: "#1A2D4C",
 };
+
 const gray = {
-  100: "#f3f7f9",
-  200: "#e7eff2",
-  300: "#bac4c8",
-  400: "#7b8f96",
-  500: "#495c62",
-  700: "#2a3b41",
-  800: "#182a2f",
-  900: "#121a1c",
+  100: "#F6FAFD",
+  200: "#EAF4FB",
+  300: "#DFEDFA",
+  400: "#D9E8FF",
+  500: "#274276",
+  600: "#203560",
+  700: "#1F324F",
+  800: "#131E35",
+  900: "#0F1626",
 };
 
 /** @type {import('tailwindcss').Config} */
